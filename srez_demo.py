bea54feec3590c6bd143a386bd242867ae42ab0e
Ben Cottier <bcottier2@gmail.com>
2019-01-30 15:41:29 +1000

    Normalise feature in demo2

--- srez_demo.py
+++ srez_demo.py
@@ -50,12 +50,20 @@ def demo2(data, num_sample):
 
         feature = list_features[index_batch]
         label = list_labels[index_batch]
+
+        print(feature.mean(), feature.var(), feature.max(), feature.min())
+        print(label.mean(), label.var(), label.max(), label.min())
     
         # Show progress with features
         feed_dict = {d.gene_minput: feature}     
         # ops = [d.gene_moutput, d.gene_mlayers, d.disc_mlayers, d.disc_moutput, d.disc_gradients]
         ops = [d.gene_moutput]
-        # ops = [d.gene_loss, d.gene_ls_loss, d.gene_dc_loss, d.disc_real_loss, d.disc_fake_loss, d.list_gene_losses]            
+        # ops = [d.gene_loss, d.gene_ls_loss, d.gene_dc_loss, d.disc_real_loss, d.disc_fake_loss, d.list_gene_losses] 
+        # dum = tf.random_normal((FLAGS.batch_size, 256, 256, 2))
+        # dum = tf.complex(dum[:,:,:,0], dum[:,:,:,1])
+        # dum = tf.abs(dum)
+        # dum = tf.reshape(dum, [FLAGS.batch_size, 256, 256, 1])
+        # ops=[dum]
         forward_passing_time = time.time()
         # gene_output, gene_layers, disc_layers, disc_output, disc_gradients = d.sess.run(ops, feed_dict=feed_dict)
         gene_output, = d.sess.run(ops, feed_dict=feed_dict)
@@ -85,11 +93,12 @@ def save_image_output(data, feature, label, gene_output,
     size = [label.shape[1], label.shape[2]]
 
     # complex input zpad into r and channel
+    complex_zpad = (feature - feature.min()) / (feature.max() - feature.min())
     complex_zpad = tf.image.resize_nearest_neighbor(feature, size)
     complex_zpad = tf.maximum(tf.minimum(complex_zpad, 1.0), 0.0)
 
     # zpad magnitude
-    mag_zpad = tf.sqrt(complex_zpad[:,:,:,0]**2+complex_zpad[:,:,:,1]**2)/tf.sqrt(2)
+    mag_zpad = tf.sqrt(complex_zpad[:,:,:,0]**2+complex_zpad[:,:,:,1]**2)/tf.sqrt(2.0)
     # mag_zpad = tf.maximum(tf.minimum(mag_zpad, 1.0), 0.0)
     mag_zpad = tf.reshape(mag_zpad, [FLAGS.batch_size, size[0], size[1]])
     # mag_zpad = tf.concat(axis=3, values=[mag_zpad, mag_zpad])
@@ -105,8 +114,8 @@ def save_image_output(data, feature, label, gene_output,
 
     # concate for visualize image
     image = tf.concat(axis=2, values=[mag_zpad, mag_output, mag_gt])
-    image = image[0:max_samples,:,:,:]
-    image = tf.concat(axis=0, values=[image[i,:,:,:] for i in range(int(max_samples))])
+    image = image[0:max_samples,:,:]
+    image = tf.concat(axis=0, values=[image[i,:,:] for i in range(int(max_samples))])
     image = d.sess.run(image)
     print('Save to image size {} type {}', image.shape, type(image))
 
